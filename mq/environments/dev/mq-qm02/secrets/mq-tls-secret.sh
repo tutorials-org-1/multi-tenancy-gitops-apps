6bc052d6a388f1eb3e5a4763a284cffbe7b9c21a
Freddie Mendoza <mendoza1@us.ibm.com>
2022-08-17 14:43:37 -0500

    tls secret changes for qm2

--- mq/environments/dev/mq-qm02/secrets/mq-tls-secret.sh
+++ mq/environments/dev/mq-qm02/secrets/mq-tls-secret.sh
@@ -3,8 +3,8 @@
 SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
 SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
 
-oc create secret tls mq-tls-secret --key="tls.key" --cert="tls.crt" --dry-run=client -o yaml > delete-mq-tls-secret.yaml
+oc create secret tls mq-tls-secret2 --key="tls.key" --cert="tls.crt" --dry-run=client -o yaml > delete-mq-tls-2.yaml
 
-kubeseal -n dev  --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-mq-tls-secret.yaml > mq-tls-secret.yaml
+kubeseal -n dev  --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-mq-tls-secret2.yaml > mq-tls-secret2.yaml
 
-rm delete-mq-tls-secret.yaml
+rm delete-mq-tls-secret2.yaml
